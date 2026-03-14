77dbdbbad2d49d5007f9db39a858ac8a29495da8
NiraOra <121713415+NiraOra@users.noreply.github.com>
2026-03-14 23:15:24 +1100

    added repo link at the footer

--- src/components/portfolio/Footer.tsx
+++ src/components/portfolio/Footer.tsx
@@ -6,9 +6,16 @@ const Footer = () => {
       <div className="container-narrow mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4 text-small">
           <p>© {currentYear} Niranjana Arun Menon</p>
-          {/* <p className="text-muted-foreground">
-            
-          </p> */}
+            <p className="text-muted-foreground">
+              Source Code:{' '}
+              <a
+                href="https://github.com/NiraOra/portfolio_nira"
+                target="_blank"
+                className="underline hover:text-primary transition-colors"
+              >
+                github.com/NiraOra/portfolio_nira
+              </a>
+            </p>
         </div>
       </div>
     </footer>
